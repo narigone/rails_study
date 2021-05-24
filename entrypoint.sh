1d72a30fbb9d61b6e8b164e29e43252170425d83
Raphael Sales <raphael.melo21@gmail.com>
2021-05-24 04:12:11 +0200

    Small fix for starting containers from scratch

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,8 @@ rm -f /myapp/tmp/pids/server.pid
 # Create database if it doesn't exist
 rake db:create
 
+# Run pending migrations
+bin/rails db:migrate
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
